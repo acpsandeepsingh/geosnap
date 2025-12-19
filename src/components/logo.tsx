1be9f2f8b2c809f6c67c3f051de7ea3b1cf7963b
acpsandeepsingh <acpsandeepsingh@gmail.com>
2025-12-19 17:01:13 +0530

    Update logo.tsx

--- src/components/logo.tsx
+++ src/components/logo.tsx
@@ -4,10 +4,11 @@ import Image from 'next/image';
 import { cn } from '@/lib/utils';
 
 export function GeoSnapLogo({ className }: { className?: string }) {
+  const basePath = '/geosnap';
   return (
     <div className={cn('relative h-8 w-auto', className)}>
       <Image
-        src="/logo.png"
+        src={`${basePath}/logo.png`}
         alt="GeoSnap Logo"
         width={120}
         height={32}
