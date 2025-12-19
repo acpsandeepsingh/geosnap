be695368a4aef7b9706b761a48f2813f7a3e36bd
acpsandeepsingh <acpsandeepsingh@gmail.com>
2025-12-19 11:32:35 +0530

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,7 @@ import type {NextConfig} from 'next';
 
 const nextConfig: NextConfig = {
   output: 'export',
+  basePath: '/geosnap',
   /* config options here */
   typescript: {
     ignoreBuildErrors: true,
@@ -11,6 +12,7 @@ const nextConfig: NextConfig = {
     ignoreDuringBuilds: true,
   },
   images: {
+    unoptimized: true,
     remotePatterns: [
       {
         protocol: 'https',
